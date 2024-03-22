fa6378eded7b95f32804ae2e0c89bf2b49b09f32
vane-hertz <vainehertz@yahoo.com>
2024-03-21 22:48:19 -0700

    completed loops and iteerating section

---
+++ preparations_exercises/loops and iterating/factorial.js
@@ -0,0 +1,16 @@
+function factorial(num) {
+  let product = 1;
+  if (num > 1) {
+    product = num * factorial(num - 1);
+  }
+  return product;
+}
+
+console.log(factorial(1));
+console.log(factorial(2));
+console.log(factorial(3));
+console.log(factorial(4));
+console.log(factorial(5));
+console.log(factorial(6));
+console.log(factorial(7));
+console.log(factorial(8));
